bc585ff3773ae9a6b31a9ee0812df4e2caa29c5f
general_suslik <glebrusyaev77@gmail.com>
2023-08-19 00:06:15 +0300

    javascript_add

--- boomnet/functional_tests.py
+++ boomnet/functional_tests.py
@@ -13,11 +13,8 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        # Edith has heard about a cool new online to-do app. She goes
-        # to check out its homepage
         self.browser.get('http://localhost:8000')
 
-        # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element()
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
@@ -26,8 +23,6 @@ class NewVisitorTest(unittest.TestCase):
 
         inputbox.send_keys('admin')
 
-        # When she hits enter, the page updates, and now the page lists
-        # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,40 +1,21 @@
-// function processPosts(){
-//     let postsContainer = document.querySelector(".fropy");
-//     let posts = postsContainer.getElementsByClassName("post");
-//     const maxHeight = "400px";
-//
-//     for (let i = 0; i < posts.length; i++){
-//         let post = posts[i];
-//
-//         let postContent = post.getElementsByClassName("post-content");
-//         postContent.style.maxHeight = maxHeight;
-//
-//         const menuButton = post.getElementById("menuButton");
-//         const menu = post.getElementById("menu");
-//         menuButton.addEventListener("click", function (){
-//             menu.classList.toggle("hidden");
-//         });
-//     }
-//
-// }
+function processPosts(){
+  const postElements = document.querySelectorAll(".post");
+  const maxHeight = "400px";
 
+  postElements.forEach(postElement => {
 
-const postElements = document.querySelectorAll(".post");
-const maxHeight = "400px";
+    const postContent = postElement.querySelector(".post-content");
+    postContent.style.maxHeight = maxHeight;
+    postContent.style.overflow = "hidden";
 
-postElements.forEach(postElement => {
-
-  const title = postElement.querySelector('.post-title').textContent;
-  const postContent = postElement.querySelector(".post-content");
-  postContent.style.maxHeight = maxHeight;
-  postContent.style.overflow = "hidden";
-
-  const menuButton = postElement.querySelector(".menuButton");
-  const menu = postElement.querySelector(".menu");
-  menuButton.addEventListener("click", function (){
-      menu.classList.toggle("hidden");
+    const menuButton = postElement.querySelector(".menuButton");
+    const menu = postElement.querySelector(".menu");
+    menuButton.addEventListener("click", function (){
+        menu.classList.toggle("hidden");
+    });
   });
+}
 
-  console.log(`Title: ${title}`);
-});
+
+
 
