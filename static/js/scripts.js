2779564025ff8322fe4b41473381818b6a664b66
general_suslik <glebrusyaev77@gmail.com>
2023-08-18 23:44:57 +0300

    javascript_add

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,17 +1,40 @@
-// menu ... button
-const menuButton = document.getElementById("menuButton");
-const menu = document.getElementById("menu");
+// function processPosts(){
+//     let postsContainer = document.querySelector(".fropy");
+//     let posts = postsContainer.getElementsByClassName("post");
+//     const maxHeight = "400px";
+//
+//     for (let i = 0; i < posts.length; i++){
+//         let post = posts[i];
+//
+//         let postContent = post.getElementsByClassName("post-content");
+//         postContent.style.maxHeight = maxHeight;
+//
+//         const menuButton = post.getElementById("menuButton");
+//         const menu = post.getElementById("menu");
+//         menuButton.addEventListener("click", function (){
+//             menu.classList.toggle("hidden");
+//         });
+//     }
+//
+// }
 
-menuButton.addEventListener("click", function (){
-    menu.classList.toggle("hidden")
-});
 
-
-//Cutting post content if it's height > 400px
-const fropy = document.querySelector(".fropy");
-const postContent = document.querySelector(".post-content");
+const postElements = document.querySelectorAll(".post");
 const maxHeight = "400px";
-postContent.style.maxHeight = maxHeight;
 
+postElements.forEach(postElement => {
 
+  const title = postElement.querySelector('.post-title').textContent;
+  const postContent = postElement.querySelector(".post-content");
+  postContent.style.maxHeight = maxHeight;
+  postContent.style.overflow = "hidden";
+
+  const menuButton = postElement.querySelector(".menuButton");
+  const menu = postElement.querySelector(".menu");
+  menuButton.addEventListener("click", function (){
+      menu.classList.toggle("hidden");
+  });
+
+  console.log(`Title: ${title}`);
+});
 
