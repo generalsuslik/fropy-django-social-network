e219aa499f9e11f8b5b2bda62f8bb26a0a0c70be
general_suslik <glebrusyaev77@gmail.com>
2023-08-19 14:00:51 +0300

    javascript_update

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,21 +1,35 @@
 function processPosts(){
   const postElements = document.querySelectorAll(".post");
-  const maxHeight = "400px";
 
   postElements.forEach(postElement => {
 
     const postContent = postElement.querySelector(".post-content");
-    postContent.style.maxHeight = maxHeight;
-    postContent.style.overflow = "hidden";
+    truncatePost(postContent)
 
-    const menuButton = postElement.querySelector(".menuButton");
-    const menu = postElement.querySelector(".menu");
-    menuButton.addEventListener("click", function (){
-        menu.classList.toggle("hidden");
-    });
+    addMenuButton(postElement);
   });
 }
 
+
+function truncatePost(element) {
+  let height = element.offsetHeight;
+  if (height > 400) {
+    element.style.maxHeight = '400px';
+    element.style.overflow = 'hidden';
+    let gradient = 'linear-gradient(to bottom, rgba(0,0,0,0), rgba(0,0,0,1) 350px)';
+    element.style.backgroundImage = gradient;
+  }
+}
+
+
+// Adding menu ... button to post
+function addMenuButton(postElement){
+  const menuButton = postElement.querySelector(".menuButton");
+  const menu = postElement.querySelector(".menu");
+  menuButton.addEventListener("click", function (){
+        menu.classList.toggle("hidden");
+    });
+}
 
 
 
